c0c30c75efb3dd1a08b41311bcc6109df95d0406
wellonefoot <wellonefoot09@gmail.com>
2026-08-28 12:48:28 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const SITE_CONFIG = {
-  shopName: 'Wellone',
-  supabaseUrl: 'https://wnavzhrkwgnegjdetdno.supabase.co',
-  supabaseAnonKey: 'sb_publishable_RbnMrDlHfEijBiejcRNPUg_mop2bqgM',
-  storageBucket: 'product-images',
-  whatsappNumber: '919746476813',
-  defaultCategoryImage: 'https://images.unsplash.com/photo-1594223274512-ad4803739b7c?auto=format&fit=crop&w=900&q=74'
-};
